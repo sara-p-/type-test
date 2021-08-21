2139eb3afd47c39d935a053eb13bb9065591f38b
sara-p- <sarapitt88@gmail.com>
2021-08-21 15:39:24 -0400

    Initial Commit

---
+++ build-utils/webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, '..', './src/js/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hello Webpack bundled JavaScript Project',
+      template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+  ],
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, '..', './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, '..', './dist'),
+  },
+  devtool: 'eval-source-map',
+}
